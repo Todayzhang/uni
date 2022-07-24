0af3c910289ca300bbad1d4705c084497e4e7fad
wanmingcheng <1114898300@qq.com>
2022-07-24 13:45:37 +0800

    添加debug插件

---
+++ uni_modules/HF-HF_debugging/pages/next/next.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<HFBox></HFBox>
+	</view>
+</template>
+
+<script>
+	import HFBox from './../../components/HFBox.vue'
+	export default {
+		components:{
+			HFBox
+		},
+		data() {
+			return {
+				tets:'测试'
+			}
+		},
+		onShow() {
+			// ||getCurrentPages().lengt<=1
+			/* uni.navigateBack({
+				delta: 1
+			}) */
+		},
+		methods: {
+			
+		},
+		onHide() {
+			uni.$emit("viewShow")
+		}
+	}
+</script>
+
+<style>
+
+</style>
